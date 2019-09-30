c003ad1c06e1c96d3ee6fa84e4f67f521331b9a2
Fu Zhang <contact@fzhang.co.uk>
2019-09-30 18:48:18 +0100

    changed name for variable of codeMirror instance

--- src/onClickEventHander.js
+++ src/onClickEventHander.js
@@ -6,9 +6,14 @@
 //document.getElementById('submit-btn-style').addEventListener('click',() =>{process()})
 
 function onSubmit(){
-   df = editor.getValue()
-   pf = editor2.getValue()
+   df = dEditor.getValue()
+   pf = pEditor.getValue()
    //console.log(df)
 
    var process = new PddlProcess(df,pf)
+}
+
+function onReset(){
+   dEditor.setValue('');
+   pEditor.setValue('');
 }
