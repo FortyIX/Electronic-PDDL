e0d3a8d6f4a88e367830426241b8d66623d60d5e
Fu Zhang <contact@fzhang.co.uk>
2019-10-01 03:05:07 +0100

    enabled shortcuts for copy and other common ones

--- main.js
+++ main.js
@@ -51,8 +51,26 @@ app.on('activate', function () {
           label: 'File',
           submenu:[
              {label: 'Save'},
-             {label: 'Load'},]
-       }]);
+             {label: 'Load'},
+            ]
+        },
+        {
+          label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+        
+        
+          
+        
+      
+      ]);
      }
      else{
       firmenu = Menu.buildFromTemplate([
